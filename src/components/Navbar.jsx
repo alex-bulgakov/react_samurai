e1a55385dd752d94036b20eb9b6613d09c800cff
alebul <master8423@gmail.com>
2021-02-24 09:06:56 +0300

    Change navbar css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,5 @@
 import React from 'react';
 import s from './Navbar.module.css';
-console.log(s);
-// let s = {
-//     'nav': 'Navbar_nav__3ou9Q',
-//     'item': 'Navbar_item__3qaF3',
-//     'active' : 'Baksndakdn_actve'
- // }
-
-let c1 = "item";
-let c2 = "active";
-// "item active"
-let classes = c1 + " " + c2;
-let classesNew = `${s.item} ${c2}`;
-
 
 const Navbar = () => {
     return <nav className={s.nav}>
