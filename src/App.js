714aea320021a3ad105c24980deeaabffede7727
alebul <master8423@gmail.com>
2021-02-25 10:57:58 +0300

    add dialogs component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import './lib/fontawesome/css/fa_all.min.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+
 
 const App = () => {
     return (
         <div className='app-wrapper'>
             <Header/>
             <Navbar/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <Dialogs />
         </div>);
 }
 
---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+const Dialogs = () => {
+    return  (
+        <span>Dialogs</span>
+    );
+}
+
+export default Dialogs;
