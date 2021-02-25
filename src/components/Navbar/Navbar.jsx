9f84dd6b16f1688e6610a8a96701b554a2817719
alebul <master8423@gmail.com>
2021-02-25 15:52:34 +0300

    add routes dialogs and profile

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,11 +4,11 @@ import s from './Navbar.module.css';
 const Navbar = () => {
     return <nav className={s.nav}>
         <ul>
-            <li className={s.item}><a href="#">Profile</a></li>
-            <li className={`${s.item} ${s.active}`}><a href="#">Messages</a></li>
-            <li className={s.item}><a href="#">News</a></li>
-            <li className={s.item}><a href="#">Music</a></li>
-            <li className={s.item}><a href="#">Settings</a></li>
+            <li className={s.item}><a href="/profile">Profile</a></li>
+            <li className={`${s.item} ${s.active}`}><a href="/dialogs">Messages</a></li>
+            <li className={s.item}><a href="/news">News</a></li>
+            <li className={s.item}><a href="/music">Music</a></li>
+            <li className={s.item}><a href="/settings">Settings</a></li>
         </ul>
     </nav>
 }
